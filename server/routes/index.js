89d97427ea69aad1d67b50ab1073eb8fc5972bdf
charliebarger <charliebarger04@gmail.com>
2022-01-17 11:51:51 -0700

    finish styles

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("memberLogin", { title: "Express" });
+  res.render("admin-log-in", { title: "Express" });
 });
 
 module.exports = router;
