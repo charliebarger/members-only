bb7ecb4f17ead8d8a201d4155e306d378db41d09
charliebarger <charliebarger04@gmail.com>
2022-01-17 14:56:53 -0700

    fix model

--- server/models/users.js
+++ server/models/users.js
@@ -1,22 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: "this field is required",
-  },
-  lastName: {
-    type: String,
-    required: "this field is required",
-  },
-  avatar: {
-    type: String,
-    required: "this field is required",
-  },
   username: {
     type: String,
     required: "this field is required",
     maxlength: 20,
+  },
+  avatarURL: {
+    type: String,
+    required: "this field is required",
   },
   password: {
     type: String,
