aefa9641d5681c63661829b02e03a8853930d6bf
charliebarger <charliebarger04@gmail.com>
2022-02-14 16:53:50 -0700

    Add confirm box for delete

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,17 @@ for (let i = 0; i < hiddenWrapper.length; i++) {
     hiddenWrapper[i].firstElementChild.classList.add("show");
   });
 }
+
+//delete function
+deleteButtons = document.querySelectorAll(".delete");
+
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this message?"
+    );
+    if (!confirm) {
+      e.preventDefault();
+    }
+  });
+});
