bbd6aa3e7d743a9f34622ed8396cb1fdf640a537
charliebarger <charliebarger04@gmail.com>
2022-02-14 13:23:00 -0700

    dont allow unauthenticated users to post messages

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -4,20 +4,24 @@ const ObjectId = require("mongodb").ObjectId;
 const { check, validationResult } = require("express-validator");
 
 exports.createMessage = async (req, res, next) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).jsonp(errors.array());
+  if (req.isAuthenticated()) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).jsonp(errors.array());
+      }
+      const user = await messageDB({
+        user: req.user._id,
+        title: req.body.messageHeader,
+        body: req.body.messageBody,
+      });
+      await messageDB.create(user);
+      res.redirect("/");
+    } catch (error) {
+      next(error);
     }
-    const user = await messageDB({
-      user: req.user._id,
-      title: req.body.messageHeader,
-      body: req.body.messageBody,
-    });
-    await messageDB.create(user);
-    res.redirect("/");
-  } catch (error) {
-    next(error);
+  } else {
+    next();
   }
 };
 
