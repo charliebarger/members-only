36141c7bcbb86605fa80045c6ae1e058af43ecd7
charliebarger <charliebarger04@gmail.com>
2022-02-14 15:34:42 -0700

    link router to index controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
+const messages = require("../controllers/indexController");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { msg: req.flash("msg") });
-});
+router.get("/", messages.retrieveMessages);
 
 module.exports = router;
