5924c3e10c32f27902d306a78ae07dd0223b5f4f
charliebarger <charliebarger04@gmail.com>
2022-02-15 12:09:38 -0700

    moved function to sing=up controller from route

--- server/controllers/sign-up.js
+++ server/controllers/sign-up.js
@@ -1,5 +1,4 @@
 const userDB = require("../models/users");
-require("../models/database");
 const { check, validationResult } = require("express-validator");
 
 exports.createUser = async (req, res, next) => {
@@ -40,4 +39,24 @@ exports.validate = (req, res) => {
     check("member").isBoolean().optional({ checkFalsy: true }),
     check("admin").isBoolean().optional({ checkFalsy: true }),
   ];
+};
+
+//function that returns a random avatar photo from a list of avatar photos
+const randomAvatars = () => {
+  const images = [
+    "http://images6.fanpop.com/image/photos/40800000/soda-me-broda-me-regular-show-xxl-40805603-842-473.png",
+    "http://tvline.com/wp-content/uploads/2011/12/5-300111230173120.jpg",
+    "https://metro.co.uk/wp-content/uploads/2018/05/sei_14554950.jpg?quality=90&strip=all",
+    "https://m.media-amazon.com/images/M/MV5BMjE1NjMyNTM3OF5BMl5BanBnXkFtZTgwMzY1NDEzMjE@._V1_.jpg",
+    "https://i.ytimg.com/vi/xUBthxxEHyM/maxresdefault.jpg",
+    "https://i.pinimg.com/originals/d8/05/1c/d8051cbf4333197b3aa715039869c4fb.jpg",
+  ];
+  return images[Math.floor(Math.random() * images.length)];
+};
+
+exports.checkUrl = (req, res, next) => {
+  if (req.body.imageUrl == "") {
+    req.body.imageUrl = randomAvatars();
+  }
+  next();
 };
--- server/routes/sign-up.js
+++ server/routes/sign-up.js
@@ -2,34 +2,12 @@ var express = require("express");
 var router = express.Router();
 const signUp = require("../controllers/sign-up");
 const { check, validationResult } = require("express-validator");
-/* GET users listing. */
+
+/* GET sign-up page. */
 router.get("/", function (req, res, next) {
-  res.render("sign-up", { title: "Express" });
+  res.render("sign-up");
 });
-
-const randomAvatars = () => {
-  const images = [
-    "http://images6.fanpop.com/image/photos/40800000/soda-me-broda-me-regular-show-xxl-40805603-842-473.png",
-    "http://tvline.com/wp-content/uploads/2011/12/5-300111230173120.jpg",
-    "https://metro.co.uk/wp-content/uploads/2018/05/sei_14554950.jpg?quality=90&strip=all",
-    "https://m.media-amazon.com/images/M/MV5BMjE1NjMyNTM3OF5BMl5BanBnXkFtZTgwMzY1NDEzMjE@._V1_.jpg",
-    "https://i.ytimg.com/vi/xUBthxxEHyM/maxresdefault.jpg",
-    "https://i.pinimg.com/originals/d8/05/1c/d8051cbf4333197b3aa715039869c4fb.jpg",
-  ];
-  return images[Math.floor(Math.random() * images.length)];
-};
-
-router.post(
-  "/",
-  (req, res, next) => {
-    if (req.body.imageUrl == "") {
-      req.body.imageUrl = randomAvatars();
-    }
-    console.log(req.body);
-    next();
-  },
-  signUp.validate(),
-  signUp.createUser
-);
+/* Post new sign up. */
+router.post("/", signUp.checkUrl, signUp.validate(), signUp.createUser);
 
 module.exports = router;
