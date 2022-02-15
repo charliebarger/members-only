02782c37638791e88de6921184d8bbce649a4a4f
charliebarger <charliebarger04@gmail.com>
2022-02-15 16:55:51 -0700

    moved form validation to server side

--- server/controllers/signUpController.js
+++ server/controllers/signUpController.js
@@ -1,6 +1,75 @@
 const userDB = require("../models/users");
 const { check } = require("express-validator");
 
+const checkUsernameAvailability = async (req, res, next) => {
+  const usernameUnavailable = await userDB.exists({
+    username: req.body.username,
+  });
+  if (usernameUnavailable) {
+    req.flash("msg", "Username is Not Available");
+    req.flash("url", req.body.imageUrl);
+    res.redirect("/sign-up");
+  }
+  next();
+};
+
+const checkPasswordLength = async (req, res, next) => {
+  if (req.body.password.length > 20 || req.body.password.length < 5) {
+    req.flash("msg", "Password must be between 5 and 20 characters");
+    req.flash("username", req.body.username);
+    req.flash("url", req.body.imageUrl);
+    res.redirect("/sign-up");
+  }
+  next();
+};
+
+const checkUsernameLength = async (req, res, next) => {
+  if (req.body.username.length > 20 || req.body.username.length < 5) {
+    req.flash("msg", "Username must be between 5 and 20 characters");
+    req.flash("url", req.body.imageUrl);
+    res.redirect("/sign-up");
+  }
+  next();
+};
+
+const checkConfirmPassword = async (req, res, next) => {
+  if (req.body.password != req.body.confirmPassword) {
+    req.flash("username", req.body.username);
+    req.flash("msg", "Passwords do not match");
+    req.flash("url", req.body.imageUrl);
+    res.redirect("/sign-up");
+  }
+  next();
+};
+
+//checks to make sure the image url is valid, if not assigns a random avatar
+const checkUrl = (req, res, next) => {
+  if (req.body.imageUrl == "") {
+    req.body.imageUrl = randomAvatars();
+  }
+  next();
+};
+
+const checkImg = (req, res, next) => {
+  const image = req.body.imageUrl.match(/(jpeg|jpg|gif|png)/) != null;
+  if (image) {
+    next();
+  } else {
+    req.flash("username", req.body.username);
+    req.flash("msg", "Image URL is not valid");
+    res.redirect("/sign-up");
+  }
+};
+
+exports.checkFormValidity = [
+  checkUsernameAvailability,
+  checkPasswordLength,
+  checkConfirmPassword,
+  checkUsernameLength,
+  checkUrl,
+  checkImg,
+];
+
 //create a new user
 exports.createUser = async (req, res, next) => {
   try {
@@ -50,12 +119,4 @@ const randomAvatars = () => {
     "https://i.pinimg.com/originals/d8/05/1c/d8051cbf4333197b3aa715039869c4fb.jpg",
   ];
   return images[Math.floor(Math.random() * images.length)];
-};
-
-//checks to make sure the image url is valid, if not assigns a random avatar
-exports.checkUrl = (req, res, next) => {
-  if (req.body.imageUrl == "") {
-    req.body.imageUrl = randomAvatars();
-  }
-  next();
 };
--- server/routes/sign-up.js
+++ server/routes/sign-up.js
@@ -5,13 +5,17 @@ const validation = require("../controllers/validationController");
 
 /* GET sign-up page. */
 router.get("/", function (req, res, next) {
-  res.render("sign-up");
+  res.render("sign-up", {
+    msg: req.flash("msg"),
+    username: req.flash("username"),
+    url: req.flash("url"),
+  });
 });
 
 /* POST new sign up. */
 router.post(
   "/",
-  signUp.checkUrl,
+  signUp.checkFormValidity,
   signUp.validate(),
   validation.checkValidation,
   signUp.createUser
