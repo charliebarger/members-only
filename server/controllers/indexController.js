7321acbb84b48724331f20d6682504a3721594fa
charliebarger <charliebarger04@gmail.com>
2022-02-16 12:42:36 -0700

    dont show messages until logged in

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -2,11 +2,12 @@ const messageDB = require("../models/messages");
 
 exports.retrieveMessages = async (req, res, next) => {
   try {
-    const posts = await messageDB
-      .find({})
-      .sort({ pinned: -1, timestamp: "desc" })
-      .populate("user");
-    res.render("index", { msg: req.flash("msg"), posts });
+    if (req.user) {
+      const posts = await messageDB.find({}).sort().populate("user");
+      res.render("index", { msg: req.flash("msg"), posts });
+    } else {
+      res.render("index", { msg: "", posts: "" });
+    }
   } catch (error) {
     next(error);
   }
